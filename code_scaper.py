1293d489548a3dca1da1020977a44fc135545cfb
bscod27 <bruno.scodari.gr@dartmouth.edu>
2022-08-23 11:12:35 -0400

    updated params

--- code_scaper.py
+++ code_scaper.py
@@ -6,17 +6,17 @@ import regex as re
 
 def code_scraper(df: pd.DataFrame, code_type: str, col_name: str) -> pd.DataFrame:
     """
-    Takes a pandas data frame with CPT or ICD-10 codes as input and 
+    Takes a pandas data frame with CPT or ICD-10 codes as input and
     outputs a pandas data frame containing the codes and their respective descriptions/summaries.
-    
-    @param df: 
-    @param code_type: 
-    @param col_name: 
+
+    :param df: Input Pandas DataFrame
+    :param code_type: String indication of 'cpt' or 'icd10'
+    :param col_name: Column header that contains the CPT or ICD-10 codes
     """
-    
+
     code = []
     summary = []
-    
+
     if code_type == 'cpt':
         for i in df[col_name]:
             url = 'https://www.aapc.com/codes/cpt-codes/' + str(i)
@@ -25,11 +25,11 @@ def code_scraper(df: pd.DataFrame, code_type: str, col_name: str) -> pd.DataFram
             string = ' '.join(re.findall('\w+', str(soup.find('p'))))
             try:
                 summary.append(re.findall('p (.*) p', string)[0])
-            except: 
+            except:
                 summary.append('Code does not exist')
-            finally: 
+            finally:
                 code.append(str(i))
-            
+
     elif code_type == 'icd10':
         for i in df[col_name]:
             a, b, c, d = str(i)[0:4]
@@ -38,12 +38,12 @@ def code_scraper(df: pd.DataFrame, code_type: str, col_name: str) -> pd.DataFram
             soup = BeautifulSoup(page.text, 'html.parser')
             try:
                 summary.append(re.findall(': (.*)<',str(soup.find('title')))[0])
-            except: 
+            except:
                 summary.append('Code does not exist')
-            finally: 
+            finally:
                 code.append(str(i))
-    
-    else: 
+
+    else:
         raise SyntaxError("Invalid code_type provided. Must be either 'cpt' or 'icd10'.")
-                
+
     return pd.DataFrame({'code':code, 'summary': summary})
